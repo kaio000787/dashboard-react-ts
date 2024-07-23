9ac6008cb67a730a3fb0ac9d6f06c58c3f3fb660
kaio000787 <166851957+kaio000787@users.noreply.github.com>
2024-07-23 16:07:41 -0300

    ...

--- src/Components/forms/Form/Form.tsx
+++ src/Components/forms/Form/Form.tsx
@@ -1,35 +1,37 @@
 import React from "react";
+import styles from './Form.module.css'
 
-import { Formik, FormikHelpers, FormikProps, FormikValues, Form as FormikForm } from "formik";
-import * as Yup from "yup";
 
-import styles from "./Form.module.css";
+import * as Yup from 'yup'
+import { Formik, FormikProps, FormikValues, Form as FormikForm, FormikHelpers } from 'formik';
 
 
 interface FormProps<T> {
-  initialValues: T;
-  validationSchema: Yup.ObjectSchema<Partial<T>>;
-  onSubmit: (values: T, formikHelpers: FormikHelpers<T>) => void | Promise<void>;
-  children: (FormikProps: FormikProps<T>) =>React.ReactNode;
+    initialValues: T
+    validationSchema: Yup.ObjectSchema<Omit<Partial<T>, "id">>
+    onSubmit: (values: T, formikHelpers: FormikHelpers<T>) => void | Promise<void>
+    children: (formikProps: FormikProps<T>) => React.ReactNode
+    enableReiniciar?: boolean;
 }
 
+
 const Form = <T extends FormikValues>({ initialValues, validationSchema, onSubmit, children }: FormProps<T>) => {
-  return (
-    <div className={styles.formwrapper}>
-       <Formik
-           initialValues={initialValues}
-           validationSchema={validationSchema}
-           onSubmit={onSubmit}
-         >
+    return (
+        <div className={styles.formWrapper}>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                enableReiniciar={true}
+                onSubmit={onSubmit}
+            >
+                {(formikProps) => (
+                    <FormikForm className={styles.form}>
+                        {children(formikProps)}
+                    </FormikForm>
+                )}
+            </Formik>
+        </div>
+    )
+}
 
-          {(formikProps) => (
-              <FormikForm className={styles.form}>
-              { children(formikProps) }
-            </FormikForm>
-          )}
-        </Formik>
-    </div>
-  );
-};
-
-export default Form;
+export default Form
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import  axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://site-pessoal-api-kr3w.onrender.com/api',
+    baseURL: 'https://site-pessoal-api-kr3w.onrender.com/api'
 });
 
 export default api  
